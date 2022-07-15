e1f1d992113153c8b896e047a5a7a34d4f2cada2
Anex <anexbensi@gmail.com>
2022-07-15 12:48:17 +0530

    basic layout

---
+++ src/Components/RowPost/RowPost.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./RowPost.css"
+
+function RowPost() {
+  return (
+    <div className='row'>
+        <h2>Title</h2>
+        <div className="posters">
+        <img className='poster' src="https://i.pinimg.com/550x/0e/4c/e0/0e4ce03e5efed43efe2986faa47dd8f8.jpg" alt="poster" />
+        <img className='poster' src="https://www.ubuy.co.in/productimg/?image=aHR0cHM6Ly9tLm1lZGlhLWFtYXpvbi5jb20vaW1hZ2VzL0kvODE0Mk5MRUkzQ0wuX0FDX1NMMTUwMF8uanBn.jpg" alt="poster" />
+        <img className='poster' src="https://m.media-amazon.com/images/I/71Lqlq3GlwL._AC_SL1500_.jpg" alt="poster" />
+        <img className='poster' src="https://i.pinimg.com/550x/0e/4c/e0/0e4ce03e5efed43efe2986faa47dd8f8.jpg" alt="poster" />
+        <img className='poster' src="https://www.ubuy.co.in/productimg/?image=aHR0cHM6Ly9tLm1lZGlhLWFtYXpvbi5jb20vaW1hZ2VzL0kvODE0Mk5MRUkzQ0wuX0FDX1NMMTUwMF8uanBn.jpg" alt="poster" />
+        <img className='poster' src="https://m.media-amazon.com/images/I/71Lqlq3GlwL._AC_SL1500_.jpg" alt="poster" />
+        <img className='poster' src="https://i.pinimg.com/550x/0e/4c/e0/0e4ce03e5efed43efe2986faa47dd8f8.jpg" alt="poster" />
+        <img className='poster' src="https://www.ubuy.co.in/productimg/?image=aHR0cHM6Ly9tLm1lZGlhLWFtYXpvbi5jb20vaW1hZ2VzL0kvODE0Mk5MRUkzQ0wuX0FDX1NMMTUwMF8uanBn.jpg" alt="poster" />
+        <img className='poster' src="https://m.media-amazon.com/images/I/71Lqlq3GlwL._AC_SL1500_.jpg" alt="poster" />
+        </div>
+        
+
+    </div>
+  )
+}
+
+export default RowPost
